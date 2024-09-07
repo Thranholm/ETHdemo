ed85e97883617478a9845aeeb30885b814be0893
Thranholm <ethranholm@hotmail.com>
2024-09-07 11:35:05 +0200

    Initial commit

---
+++ R/time2.R
@@ -0,0 +1,13 @@
+#' @title Times number by 2
+#'
+#' @param x A numeric value
+#'
+#' @returns Output the input number multiplied by 2
+#' @export
+#'
+#' @examples
+#' times2(2)
+
+times2 <- function(x){
+  x*2
+}
